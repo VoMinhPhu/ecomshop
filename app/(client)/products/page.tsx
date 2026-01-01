3ff684925b7781e31cfbc8f2d55b50424eddf160
VoMinhPhu <vominhphu2003@gmail.com>
2026-01-01 16:21:54 +0700

    FIX: useSearchParam on product page

--- app/(client)/products/page.tsx
+++ app/(client)/products/page.tsx
@@ -1,11 +1,14 @@
+import { Suspense } from 'react';
 import ListProducts from '@/components/products/ListProducts';
 import FilterProductPage from '@/components/products/FilterProductPage';
 
 const Page = () => {
   return (
     <div className="lg:max-w-300 mx-auto">
-      <FilterProductPage />
-      <ListProducts />
+      <Suspense fallback={null}>
+        <FilterProductPage />
+        <ListProducts />
+      </Suspense>
     </div>
   );
 };
