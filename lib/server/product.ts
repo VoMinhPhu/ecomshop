051178a6599a91db7199ec87a557fc51c61a5e1e
VoMinhPhu <vominhphu2003@gmail.com>
2026-01-03 08:53:51 +0700

    ADD:: fallback for block on home page

--- lib/server/product.ts
+++ lib/server/product.ts
@@ -2,43 +2,53 @@ import { Category } from '@/types/categories';
 import { NewProductsResponseType, TopSellResponseType } from '@/types/products';
 
 export async function getTopSell(): Promise<TopSellResponseType> {
-  const res = await fetch(`${process.env.API_URL}/orders/top-sell`, {
-    next: { revalidate: 60 * 2 }, // ISR 120s
-  });
-
-  if (!res.ok) throw new Error('Failed to fetch top sell');
-
-  return res.json();
+  try {
+    const res = await fetch(`${process.env.API_URL}/orders/top-sell`, {
+      next: { revalidate: 60 * 2 },
+    });
+    if (!res.ok) throw new Error('Failed to fetch top sell');
+    return res.json();
+  } catch (err) {
+    console.warn('Fetch top sell failed:', err);
+    return [];
+  }
 }
 
 export async function getNewProduct(): Promise<NewProductsResponseType> {
-  const res = await fetch(`${process.env.API_URL}/product/new`, {
-    next: { revalidate: 60 * 10 }, // ISR 600s
-  });
-
-  if (!res.ok) throw new Error('Failed to fetch new product');
-
-  return res.json();
+  try {
+    const res = await fetch(`${process.env.API_URL}/product/new`, {
+      next: { revalidate: 60 * 10 },
+    });
+    if (!res.ok) throw new Error('Failed to fetch new product');
+    return res.json();
+  } catch (err) {
+    console.warn('Fetch new product failed:', err);
+    return [];
+  }
 }
 
 export async function getDiscountProducts(): Promise<NewProductsResponseType> {
-  const res = await fetch(`${process.env.API_URL}/product/discount`, {
-    next: { revalidate: 60 * 5 }, // ISR 300s
-  });
-
-  if (!res.ok) throw new Error('Failed to fetch discount product');
-
-  return res.json();
+  try {
+    const res = await fetch(`${process.env.API_URL}/product/discount`, {
+      next: { revalidate: 60 * 5 },
+    });
+    if (!res.ok) throw new Error('Failed to fetch discount product');
+    return res.json();
+  } catch (err) {
+    console.warn('Fetch discount product failed:', err);
+    return [];
+  }
 }
 
 export async function getAllCategories(): Promise<Category[]> {
-  const res = await fetch(`${process.env.API_URL}/category/all`, {
-    next: {
-      revalidate: 60 * 60 * 12, // 12h
-      tags: ['categories'],
-    },
-  });
-
-  if (!res.ok) throw new Error('Failed to fetch all category');
-  return res.json();
+  try {
+    const res = await fetch(`${process.env.API_URL}/category/all`, {
+      next: { revalidate: 60 * 60 * 12, tags: ['categories'] },
+    });
+    if (!res.ok) throw new Error('Failed to fetch all category');
+    return res.json();
+  } catch (err) {
+    console.warn('Fetch categories failed:', err);
+    return [];
+  }
 }
