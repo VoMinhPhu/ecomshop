1cdf041a188cdf32afed0146cfcb97e4f640b961
VoMinhPhu <vominhphu2003@gmail.com>
2026-05-03 14:55:16 +0700

    UPDATE: gap card on home page on mobile

--- components/home/DiscountProducts.tsx
+++ components/home/DiscountProducts.tsx
@@ -7,14 +7,14 @@ export default async function DiscountProducts() {
   const products = await getDiscountProducts();
 
   return (
-    <div className="bg-gradient-to-b from-red-400 to-white mt-4 rounded-md p-4 pb-12 gap-2">
+    <div className="bg-gradient-to-b from-red-400 to-white mt-4 rounded-md md:p-4 p-2 md:pb-12 pb-6 gap-2">
       <div className="flex items-center justify-between">
         <p className="font-bold text-lg">Giá tốt hôm nay</p>
         <Link href="/products" className="text-sm font-semibold">
           Xem tất cả
         </Link>
       </div>
-      <div className="grid lg:grid-cols-5 md:grid-cols-3 grid-cols-2 gap-x-1.5 gap-y-2 mt-8">
+      <div className="grid lg:grid-cols-5 md:grid-cols-3 grid-cols-2 md:gap-x-1.5 gap-0.75 md:gap-y-2 gap-y-1 md:mt-8 mt-2">
         {products.map((p) => (
           <DiscountProductCard key={p.id} product={p} />
         ))}
--- components/home/NewProducts.tsx
+++ components/home/NewProducts.tsx
@@ -6,14 +6,14 @@ export default async function NewProducts() {
   const products = await getNewProduct();
 
   return (
-    <div className="bg-white mt-4 rounded-md p-4 pb-12 gap-2">
+    <div className="bg-white mt-4 rounded-md md:p-4 p-2 md:pb-12 pb-6 gap-2">
       <div className="flex items-center justify-between">
         <p className="font-bold text-lg">Những sản phẩm mới của cửa hàng</p>
         <Link href="/products?sort=newest" className="text-primary text-sm font-semibold">
           Xem tất cả
         </Link>
       </div>
-      <div className="grid lg:grid-cols-5 md:grid-cols-3 grid-cols-2 gap-x-1.5 gap-y-2 mt-8">
+      <div className="grid lg:grid-cols-5 md:grid-cols-3 grid-cols-2 md:gap-x-1.5 gap-0.75 md:gap-y-2 gap-y-1 md:mt-8 mt-2">
         {products.map((p) => (
           <ProductCard key={p.id} product={p} />
         ))}
