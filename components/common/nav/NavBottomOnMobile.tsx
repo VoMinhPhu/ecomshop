2f141895d9f33eece6830225deba3a09374d4584
VoMinhPhu <vominhphu2003@gmail.com>
2026-05-04 13:14:36 +0700

    FIX: hidden filter menu on tablet, pc

--- components/common/nav/NavBottomOnMobile.tsx
+++ components/common/nav/NavBottomOnMobile.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { HomeIcon, LayersIcon, Package2Icon, ShoppingCartIcon, UserIcon } from 'lucide-react';
 
